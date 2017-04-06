d29a06c3581268be9dae2ef9127d72ce027350c6
Dmitriy Groschovskiy <groschovskiy@google.com>
2017-04-06 20:32:42 +0300

    New information screens for pharm app
    1. Added preparate information view
    2. Added application information view

---
+++ graduation-project/app/src/main/java/com/example/graduation_project/information/AppInformation.java
@@ -0,0 +1,15 @@
+package com.example.graduation_project.information;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.graduation_project.R;
+
+public class AppInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appinfo);
+    }
+}
